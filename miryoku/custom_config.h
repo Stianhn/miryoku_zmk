6d9197070b8567de8d6f755b9a579a509c0eb6bc
Stian H. Nesbø <stian.n@gmail.com>
2025-04-22 11:25:13 -0500

    fux

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -42,7 +42,6 @@ MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
 MIRYOKU_X(GAME,   "Game") \
-MIRYOKU_X(GAMENUM,"GNum")
 
 #define MIRYOKU_LAYERMAPPING_GAME( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -55,7 +54,6 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYERMAPPING_GAMENUM(...) MIRYOKU_MAPPING(__VA_ARGS__)
 
 #define U_BASE        0
 #define U_EXTRA       1
